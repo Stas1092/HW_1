7bb3107d7581530636f2fc053a00ac24f28b57e3
Stas1092 <ss9262694394@gmail.com>
2023-02-23 15:55:33 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 12345;
+
+        int y = 20;
+
+        int z = (x / y);
+
+        System.out.println("Бонусных миль: " + z);
+    }
+}
